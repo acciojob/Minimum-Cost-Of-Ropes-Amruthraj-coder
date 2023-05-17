41f96af96265bcc6ff6aa7aee001835927be7b0a
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2023-05-17 23:31:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function calculateMinCost() {
 	let sum=0;
  
 while(arr.length!=1){
-arr.sort(function(a,b){return a-b} )
+arr.sort(function(a,b){return a>b} )
 // console.log(arr)
 sum+=arr[0]+arr[1];
 let n=arr[0]+arr[1];
