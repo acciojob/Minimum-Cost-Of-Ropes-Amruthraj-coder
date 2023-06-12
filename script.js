315e24ba4853feb1ccc403395c2bbca2edcebc76
Amruthraj-coder <122180037+Amruthraj-coder@users.noreply.github.com>
2023-06-13 00:41:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function calculateMinCost() {
-  const str = document.getElementById("rope-lengths").value.trim();
-	
-	const arr = str.trim().split(",").map(x=>x.trim().parseInt(x));
+  const str = document.getElementById("rope-lengths").value;
+	const arr = str.split(",").map(Number);
+	//console.log(arr);
 	let sum=0;
  
-while(arr.length!=1){
-arr.sort(function(a,b){return a>b} )
-// console.log(arr)
+while(arr.length>1){
+arr.sort(function(a,b){return a-b} )
+console.log(arr)
 sum+=arr[0]+arr[1];
 let n=arr[0]+arr[1];
 arr.splice(0,2,n);
 // console.log(arr);
 }
-// return sum;
+
   
   
    document.getElementById("result").innerText =sum
